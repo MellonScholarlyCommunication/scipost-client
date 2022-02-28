e240d4cd4fdfe1e8f4351e94513cd933e812ec29
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2022-02-28 16:54:47 +0100

    Adding the Accept from SciPost back to the preprint

--- bin/scipost2events.groovy
+++ bin/scipost2events.groovy
@@ -15,6 +15,7 @@ new SciPostClient().cache_loop(submissionsFile, {
 
 submissionCollector.each { entry -> 
     preprintOfferProcessor(entry.value) 
+    acceptOfferProcessor(entry.value)
 }
 
 def submissionCollect(submission, collector) {
@@ -38,8 +39,8 @@ def preprintOfferProcessor(thread) {
         def thread_id  = submission['thread_hash']
         def thread_seq = submission['thread_sequence_order']
 
-        def id         = Util.makeActivityId(preprint,"${thread_id}.${thread_seq}")
         def object_id  = Util.makePreprintUrl(preprint)
+        def id         = Util.makeActivityId(object_id,"${thread_id}.${thread_seq}")
         def authors    = Util.parseAuthor(submission['author_list'])
         def actor_id   = Util.webidLookup(authors[0])
         def origin     = Util.originLookup(authors[0])
@@ -62,6 +63,70 @@ def preprintOfferProcessor(thread) {
             ] ,
             'origin' : origin ,
             'target' : target
+        ]
+
+        def json = JsonOutput.toJson(event)
+
+        def outputFile = "output/" + makeEventFile(id)
+
+        System.err.println("Writing to ${outputFile}")
+        
+        createEventDir(outputFile)
+
+        new File(outputFile).write(
+           JsonOutput.prettyPrint(json) 
+        )
+    }
+}
+
+// Accept Offer takes all submissons and act as if
+// for each of them an Accept was send back to the author
+def acceptOfferProcessor(thread) {
+    for (submission in thread) {
+        def service    = "https://scipost.org/submissons"
+
+        def preprint   = submission['preprint']['url'].replaceAll("http:","https:")
+        def thread_id  = submission['thread_hash']
+        def thread_seq = submission['thread_sequence_order']
+
+        def id         = Util.makeActivityId(service,"${thread_id}.${thread_seq}")
+        def object_id  = Util.makePreprintUrl(preprint)
+        def inReplyTo  = Util.makeActivityId(object_id,"${thread_id}.${thread_seq}")
+        def offer_id   = Util.makeActivityId(object_id,"${thread_id}.${thread_seq}")
+        def authors    = Util.parseAuthor(submission['author_list'])
+        def target_id  = Util.webidLookup(authors[0])
+        def cite_as    = Util.makeDOI(object_id)
+
+        def event = [
+            '@context' : 'https://www.w3.org/ns/activitystreams' ,
+            'id' : id ,
+            'type' : 'Accept' ,
+            'actor' : [
+                'id'   : 'https://scipost.org/person/admin/profile/card#me' ,
+                'name' : 'Scipost autoreply robot' ,
+                'type' : 'Software'
+            ] ,
+            'context' : [
+                'id' : object_id ,
+                'ietf:cite-as': cite_as ,
+                'type' : 'Document'
+            ] ,
+            'inReplyTo': inReplyTo,
+            'object' : [
+                'id' : inReplyTo ,
+                'object' : object_id ,
+                'type': 'Offer'
+            ] ,
+            'origin' : [
+                'id' : 'https://scipost.org/profile/card#me',
+                'name': 'Scipost service',
+                'type': 'Application'
+            ] ,
+            'target' : [
+                'id'   : target_id ,
+                'name' : authors[0] ,
+                'type' : 'Person' 
+            ]
         ]
 
         def json = JsonOutput.toJson(event)
